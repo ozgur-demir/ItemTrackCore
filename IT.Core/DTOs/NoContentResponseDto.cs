157f93809b13e7db69afb7435faf8e7cd0f5512a
ozgur-demir <info@csyolu.com>
2022-06-11 04:11:14 +0300

    CustomResponseDto added

---
+++ IT.Core/DTOs/NoContentResponseDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace IT.Core.DTOs
+{
+    public class NoContentResponseDto
+    {
+        public List<String> Errors { get; set; }
+        [JsonIgnore]
+        public int StatusCode { get; set; }
+        public static NoContentResponseDto Success(int statusCode)
+        {
+            return new NoContentResponseDto { StatusCode = statusCode };
+        }
+        public static NoContentResponseDto Fail(int statusCode, List<String> errors)
+        {
+            return new NoContentResponseDto { StatusCode = statusCode, Errors = errors };
+        }
+        public static NoContentResponseDto Fail(int statusCode, string error)
+        {
+            return new NoContentResponseDto { StatusCode = statusCode, Errors = new List<string> { error } };
+        }
+}
